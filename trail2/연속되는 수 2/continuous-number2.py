db84807f264dacff89bf3e87db729f025263610c
P-WJ <pwj1103@gmail.com>
2026-05-29 20:20:32 +0900

    [Passed] 연속되는 수 2 (69ms, 17MB)

---
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+cnt = 1
+max_cnt = 0
+for i in range(n-1):
+    if arr[i] == arr[i+1]:
+        cnt += 1     
+    else:
+        max_cnt = max(max_cnt, cnt)
+        cnt = 1
+
+max_cnt = max(max_cnt, cnt)
+print(max_cnt)
