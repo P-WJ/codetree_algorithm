742938a4f1ee304e6052b392fde9ce615255c98e
P-WJ <pwj1103@gmail.com>
2026-06-19 03:20:14 +0900

    [Passed] 거울에 레이저 쏘기 2 (52ms, 16MB)

--- trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -4,13 +4,11 @@ k = int(input())
 
 # Please write your code here.
 r, c, d = 0, 0, 0
-
 dr = [1, 0, -1, 0]
 dc = [0, -1, 0, 1]
 
 a = [1, 0, 3, 2] # /
 b = [3, 2, 1, 0] # \
-
 
 if (k-1) // n == 0:
     r, c = 0, (k-1) % n
@@ -28,18 +26,17 @@ elif (k-1) // n == 3:
     r, c = (n-1) - (k-1) % n, 0
     d = 3
 
-
 cnt = 0
 
 while 0 <= r < n and 0 <= c < n:
     if grid[r][c] == "/":
         r, c = r + dr[a[d]], c + dc[a[d]]
         d = a[d]
-
+    
     elif grid[r][c] == "\\":
         r, c = r + dr[b[d]], c + dc[b[d]]
         d = b[d]
-    
+
     cnt += 1
 
 print(cnt)
