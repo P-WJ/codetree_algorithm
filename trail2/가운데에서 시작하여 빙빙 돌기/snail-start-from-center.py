d8e5086722cc61dd3d0515cd1a9b2788925c1344
P-WJ <pwj1103@gmail.com>
2026-06-19 16:49:18 +0900

    [Passed] 가운데에서 시작하여 빙빙 돌기 (78ms, 18MB)

--- trail2/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ trail2/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -6,26 +6,25 @@ grid = [[0] * n for _ in range(n)]
 dir = [[0,1], [-1,0], [0,-1], [1,0]]
 
 k = n // 2
-r,c,d = k,k,-1
+r,c,d = k,k,0
 
 grid[k][k] = 1
 
 for i in range(2, n*n+1):
-    nr, nc = r + dir[d][0], c + dir[d][1]
 
-    if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
-        
-        if grid[r + dir[(d+1)%4][0]][c + dir[(d+1)%4][1]] == 0:
-            d = (d+1) % 4
-            nr, nc = r + dir[d][0], c + dir[d][1]
-
-    else:
-        d = (d+1) % 4
-        nr, nc = r + dir[d][0], c + dir[d][1]
-
-    r, c = nr, nc
+    # 이동 먼저
+    r += dir[d][0]
+    c += dir[d][1]
     grid[r][c] = i
 
+    # 방향 전환
+    nd = (d+1) % 4
+    nr = r + dir[nd][0]
+    nc = c + dir[nd][1]
+
+    # 방향 전환 유효 확인
+    if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
+        d = nd
 
 for i in range(n):
     for j in range(n):
