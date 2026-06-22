20aeeecbad8b072280ed8f3cedd25c4a8af359b4
P-WJ <pwj1103@gmail.com>
2026-06-22 14:28:13 +0900

    [Passed] 체크판위에서 2 (56ms, 16MB)

--- trail2/체크판위에서 2/on-the-checkboard-2.py
+++ trail2/체크판위에서 2/on-the-checkboard-2.py
@@ -1,5 +1,5 @@
 R, C = map(int, input().split())
-arr = [list(input().split()) for _ in range(R)]
+grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 ans = 0
@@ -7,19 +7,19 @@ ans = 0
 for a in range(1, R-1):
     for b in range(1, C-1):
         
-        if arr[0][0] == arr[a][b]:
+        if grid[a][b] == grid[0][0]:
             continue
-
 
         for c in range(a+1, R-1):
             for d in range(b+1, C-1):
 
-                if arr[a][b] == arr[c][d]:
+                if grid[a][b] == grid[c][d]:
                     continue
 
-                if arr[c][d] == arr[R-1][C-1]:
+                if grid[c][d] == grid[R-1][C-1]:
                     continue
 
                 ans += 1
+
 
 print(ans)
