87f5a51356a42993c3ba0a3895b729ce599abd95
P-WJ <pwj1103@gmail.com>
2026-06-22 22:52:12 +0900

    [Passed] 체크판위에서 2 (55ms, 16MB)

--- trail2/체크판위에서 2/on-the-checkboard-2.py
+++ trail2/체크판위에서 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ grid = [list(input().split()) for _ in range(R)]
 # Please write your code here.
 ans = 0
 
-for a in range(1, R-1):
-    for b in range(1, C-1):
+for a in range(1, R):
+    for b in range(1, C):
         
         if grid[a][b] == grid[0][0]:
             continue
