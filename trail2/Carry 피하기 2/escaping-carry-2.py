7668625dcccae3fdd38bd94bad113b4286bbd86a
P-WJ <pwj1103@gmail.com>
2026-06-22 16:53:45 +0900

    [Passed] Carry 피하기 2 (49ms, 16MB)

--- trail2/Carry 피하기 2/escaping-carry-2.py
+++ trail2/Carry 피하기 2/escaping-carry-2.py
@@ -2,41 +2,25 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-ans  = -1
 
-for i in range(n):
-    if len(str(arr[i])) <= 6:
-        
-        arr[i] =  "0"*(6 - len(str(arr[i]))) + str(arr[i])
+def carry(a, b, c):
+    
+    while a or b or c:
+        if a % 10 + b % 10 + c % 10 >= 10:
+            return True
+        a //= 10
+        b //= 10
+        c //= 10
 
+    return False
+    
 
-for i in range(0, n-2):
-    s = 0
+ans = -1
+
+for i in range(n-2):
     for j in range(i+1, n-1):
-
         for k in range(j+1, n):
-            
-
-            if int(arr[i][-1]) + int(arr[j][-1]) + int(arr[k][-1]) >= 10:
-                continue
-
-            elif int(arr[i][-2]) + int(arr[j][-2]) + int(arr[k][-2]) >= 10:
-                continue
-            
-            elif int(arr[i][-3]) + int(arr[j][-3]) + int(arr[k][-3]) >= 10:
-                continue
-            
-            elif int(arr[i][-4]) + int(arr[j][-4]) + int(arr[k][-4]) >= 10:
-                continue
-
-            elif int(arr[i][-5]) + int(arr[j][-5]) + int(arr[k][-5]) >= 10:
-                continue
-
-            elif int(arr[i][-6]) + int(arr[j][-6]) + int(arr[k][-6]) >= 10:
-                continue
-
-            s = int(arr[i]) + int(arr[j]) + int(arr[k])
-
-            ans = max(s, ans)
+            if not carry(arr[i], arr[j], arr[k]):
+                ans = max(ans, arr[i] + arr[j] + arr[k])
 
 print(ans)
