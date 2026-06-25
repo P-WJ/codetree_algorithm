080d10f418b0419fed6adcbdbb470dc5c584ad06
P-WJ <pwj1103@gmail.com>
2026-06-26 04:58:29 +0900

    [Passed] 상해버린 치즈 (59ms, 17MB)

--- trail2/상해버린 치즈/rotten-cheese.py
+++ trail2/상해버린 치즈/rotten-cheese.py
@@ -15,21 +15,22 @@ for _ in range(S):
 
 # Please write your code here.
 
-# sick_p(아픈사람)이 sick_t(아픈 시간)전에 먹은 치즈 => 이 치즈 먹은 사람들 몇명?
+# => 각 배열의 교집합 구함(ex. [[1,4,2].[1,2]] -> [1,2]) => 교집합해서 나온 치즈를 먹은 사람들의 수.
 
-sick_ch = [] # 상한 치즈
-
+sick_ch = []
 for i in range(S):
     li = []
     for j in range(D):
 
-        if p[j] == sick_p[i] and t[j] < sick_t[i]: 
+        # sick_p(아픈사람)이 sick_t(아픈 시간)전에 먹은 치즈 각각 배열에 저장
+        if p[j] == sick_p[i] and t[j] < sick_t[i]:  
             li.append(m[j])
 
     sick_ch.append(li)
 
-arr1 = []
 
+# sick_ch의 교집합 구함
+arr1 = []
 for ch in sick_ch[0]:
     f = True
     
@@ -41,8 +42,9 @@ for ch in sick_ch[0]:
     if f:
         arr1.append(ch)
 
-arr = []
 
+# 교집합 해서 나온 치즈를 먹은 사람의 수
+arr = []
 for i in range(D):
     if m[i] in arr1: 
         if p[i] not in arr:
