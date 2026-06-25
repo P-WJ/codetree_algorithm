fe90a4ad2a25f6eef241977829bac22afe68bbf5
P-WJ <pwj1103@gmail.com>
2026-06-25 23:03:34 +0900

    [Passed] 가장 가까운 두 점 사이의 거리 (49ms, 16MB)

---
+++ trail2/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+ans = sys.maxsize
+
+for i in range(n-1):
+    for j in range(i+1, n):
+        d = pow(x[i]-x[j], 2) + pow(y[i]-y[j], 2)
+        ans = min(ans, d)
+
+print(ans)
