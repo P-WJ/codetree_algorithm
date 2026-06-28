c07035dd64cba691d0d386a8179b480c802ffd0e
P-WJ <pwj1103@gmail.com>
2026-06-29 00:07:14 +0900

    [Passed] 등차수열 (69ms, 17MB)

--- trail2/등차수열/arithmetic-sequence.py
+++ trail2/등차수열/arithmetic-sequence.py
@@ -7,14 +7,12 @@ a = list(map(int, input().split()))
 ans = 0
 
 for i in range(1, max(a)):
-    arr = []
     cnt = 0
+
     for j in range(n):
-        arr.append(abs(i-a[j]))
-    for k in Counter(arr).values():
-        if k == 2:
-            cnt += 1
+        for k in range(j+1, n):
+            if a[j] + a[k] == 2 * i:
+                cnt += 1
 
     ans = max(ans, cnt)
-
 print(ans)
