0b479d4fb155b70fb1457d7c6733fb9bb0b842f0
P-WJ <pwj1103@gmail.com>
2026-07-12 17:23:15 +0900

    [Passed] 독서실의 거리두기 4 (90ms, 18MB)

--- trail2/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ trail2/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,34 +1,22 @@
-import sys
-
 n = int(input())
 seat = list(input())
 
 # Please write your code here.
-empty = []
-for i in range(n):
-    if seat[i] == "0":
-        empty.append(i)
-
 ans = 0
 
-for i in range(len(empty)):
-    for j in range(i+1, len(empty)):
-        tmp = seat[:]
-        tmp[empty[i]] = "1"
-        tmp[empty[j]] = "1"
+for i in range(n):
+    for j in range(i+1, n):
+        if seat[i] == "0" and seat[j] == "0":
+            seat[i] = seat[j] = "1"
+            
+            d = n
+            for a in range(n):
+                for b in range(a+1, n):
+                    if seat[a] == "1" and seat[b] == "1":
+                        d = min(d, b-a)
 
-        people = []
+            ans = max(ans, d)
 
-        for k in range(n):
-            if tmp[k] == "1":
-                people.append(k)
-
-        min_d = sys.maxsize
-
-        for a in range(len(people)):
-            for b in range(a+1, len(people)):
-                min_d = min(min_d, people[b] - people[a])
-
-        ans = max(ans, min_d)
+            seat[i] = seat[j] = "0"
 
 print(ans)
