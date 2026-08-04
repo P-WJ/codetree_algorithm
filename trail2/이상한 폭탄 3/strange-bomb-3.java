374163e47154d6d4166f141407fe5762b8fd5916
P-WJ <pwj1103@gmail.com>
2026-08-04 17:23:41 +0900

    [Passed] 이상한 폭탄 3 (132ms, 10MB)

--- trail2/이상한 폭탄 3/strange-bomb-3.java
+++ trail2/이상한 폭탄 3/strange-bomb-3.java
@@ -3,51 +3,54 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int k = sc.nextInt();
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++)
+            bombs[i] = sc.nextInt();
+        // Please write your code here.
 
-        int[] bomb = new int[n];
 
+        int ans = 0;
+        int maxCnt = 0;
+        
         for (int i = 0; i < n; i++) {
-            bomb[i] = sc.nextInt();
-        }
-
-        int answer = 0;
-        int max = 0;
-
-        for (int i = 0; i < n; i++) {
-
+            
             boolean explode = false;
 
-            // 거리 K 안에 같은 번호가 있는지 확인
             for (int j = 0; j < n; j++) {
-                if (i == j) continue;
+                if (i == j) {
+                    continue;
+                }
 
-                if (bomb[i] == bomb[j] && Math.abs(i - j) <= k) {
+                if (bombs[i] == bombs[j] && Math.abs(i-j) <= k) {
                     explode = true;
                     break;
                 }
             }
 
-            if (!explode) continue;
+            if (!explode) {
+                continue;
+            }
 
-            // 같은 번호 전체 개수 세기
             int cnt = 0;
+
             for (int j = 0; j < n; j++) {
-                if (bomb[j] == bomb[i]) {
+                if (bombs[i] == bombs[j]) {
                     cnt++;
                 }
             }
-
-            if (cnt > max) {
-                max = cnt;
-                answer = bomb[i];
-            } else if (cnt == max && bomb[i] > answer) {
-                answer = bomb[i];
+            
+            if (maxCnt < cnt) {
+                maxCnt = cnt;
+                ans = bombs[i];
+            } else if (maxCnt == cnt && ans < bombs[i]) {
+                ans = bombs[i];
             }
-        }
+            
 
-        System.out.println(answer);
+        }
+        System.out.println(ans);
+        
     }
 }
