23d1d2d583707087c03893da2a700539c23c40a4
P-WJ <pwj1103@gmail.com>
2026-08-06 22:17:31 +0900

    [Passed] 최대 H 점수 2 (125ms, 10MB)

---
+++ trail2/최대 H 점수 2/maximum-h-score-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        // Please write your code here.
+
+        int ans = 0;
+
+        for (int i = 1; i <= 100; i++) {
+
+            int cnt = 0;
+            int cntl = 0;
+
+            for (int j = 0; j < n; j++) {
+                
+                if (a[j] >= i) {
+                    cnt++;
+                } else if (a[j] == i-1) {
+                    if (cntl < l) {
+                        cnt++;
+                        cntl++;
+                    }
+                }
+            }
+
+            if (cnt >= i) {
+                ans = i;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
