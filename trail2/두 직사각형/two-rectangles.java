849a1a402dfd19abf5af290b3f8c428a22248a8e
P-WJ <pwj1103@gmail.com>
2026-08-07 10:58:36 +0900

    [Passed] 두 직사각형 (128ms, 10MB)

---
+++ trail2/두 직사각형/two-rectangles.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+        
+        // Please write your code here.
+        if (x2 < a1 || x1 > a2 || y2 < b1 || y1 > b2) {
+                System.out.println("nonoverlapping");
+            } else {
+                System.out.println("overlapping");
+            }
+    }
+}
