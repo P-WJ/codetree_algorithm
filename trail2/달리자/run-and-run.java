bb8977d82f2466f61aea204ca1600c7b1e69e0d8
P-WJ <pwj1103@gmail.com>
2026-08-07 12:08:16 +0900

    [Passed] 달리자 (130ms, 10MB)

---
+++ trail2/달리자/run-and-run.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        int ans = 0;
+        int prefix = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix += a[i] - b[i];
+            ans += prefix;
+        }
+    System.out.println(ans);
+    }
+}
