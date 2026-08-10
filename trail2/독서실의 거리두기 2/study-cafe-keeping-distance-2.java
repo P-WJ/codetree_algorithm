acdf36f73e4fa00f5d3c2c0e2b01d0ea4452e61f
P-WJ <pwj1103@gmail.com>
2026-08-10 20:45:24 +0900

    [Passed] 독서실의 거리두기 2 (145ms, 10MB)

--- trail2/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ trail2/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -6,39 +6,41 @@ public class Main {
         String seats = sc.next();
         // Please write your code here.
 
-
         int ans = 0;
 
         int existMinD = Integer.MAX_VALUE;
-        // 기존 자리 거리 최소
-        for (int i = 0; i < n-1; i++) {
-                        
+        // 기존 자리 최소거리
+        for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
-                if (i == j) continue;
                 if (seats.charAt(i) == '1' && seats.charAt(j) == '1') {
-                    int d = Math.abs(i - j);
+                    int d = j-i;
                     existMinD = Math.min(existMinD, d);
                 }
-            }
+            }    
         }
 
-        // 새 자리 거리 최소
+        // 새 자리 최소 거리
         for (int i = 0; i < n; i++) {
             if (seats.charAt(i) == '0') {
+    
                 int newMinD = Integer.MAX_VALUE;
 
                 for (int j = 0; j < n; j++) {
                     if (seats.charAt(j) == '1') {
-                        int d = Math.abs(i - j);
+                        
+                        int d = Math.abs(i-j);
                         newMinD = Math.min(newMinD, d);
+
                     }
                 }
-
-                // 기존, 새 자리 거리 최소
+                // 기존 자리와 새 자리의 최소거리 비교
                 int minD = Math.min(existMinD, newMinD);
+
+                // 가까운 두 사람 간 거리 중 최대
                 ans = Math.max(ans, minD);
-            }     
+            }
         }
+
         System.out.println(ans);
     }
 }
