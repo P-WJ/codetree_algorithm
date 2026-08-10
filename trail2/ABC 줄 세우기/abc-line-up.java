b7225c812de7e94723ffc4fa4936c71baa74b4a9
P-WJ <pwj1103@gmail.com>
2026-08-10 21:58:08 +0900

    [Passed] ABC 줄 세우기 (136ms, 10MB)

--- trail2/ABC 줄 세우기/abc-line-up.java
+++ trail2/ABC 줄 세우기/abc-line-up.java
@@ -12,16 +12,15 @@ public class Main {
         int ans = 0;
 
         for (int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (arr[i] > arr[j]) {
-                    char temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    char temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                     ans++;
                 }
             }
         }
-
         System.out.print(ans);
     }
 }
