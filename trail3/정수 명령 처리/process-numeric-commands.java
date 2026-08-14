ba9ff8b0c5163e486249527bfd70dd7ec00942e8
P-WJ <pwj1103@gmail.com>
2026-08-14 17:38:34 +0900

    [Passed] 정수 명령 처리 (373ms, 17MB)

---
+++ trail3/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        Stack<Integer> stack = new Stack<>();
+    
+        for (int i = 0; i < n; i++) {
+
+            String cmd = sc.next();
+            
+            if (cmd.equals("push")) {
+                int k = sc.nextInt();
+                stack.push(k);
+            }
+
+            else if (cmd.equals("size")) {
+                System.out.println(stack.size());
+            }
+
+            else if (cmd.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            }
+
+            else if (cmd.equals("pop")) {
+                System.out.println(stack.pop());
+            }
+
+            else if (cmd.equals("top")) {
+                System.out.println(stack.peek());
+            }
+        }
+    }
+}
