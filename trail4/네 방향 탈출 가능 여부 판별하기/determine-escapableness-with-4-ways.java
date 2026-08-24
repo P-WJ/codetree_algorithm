634e505b2ca44e02edcda8fb363a9c77830a00d2
P-WJ <pwj1103@gmail.com>
2026-08-24 14:27:12 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (95ms, 9MB)

--- trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -1,32 +1,40 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     static int n, m;
     static int[][] grid;
     static boolean[][] visited;
 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         grid = new int[n][m];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                grid[i][j] = sc.nextInt();
-
         visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0 ; j < m; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
         Queue<int[]> q = new ArrayDeque<>();
 
-        q.add(new int[]{0, 0});
+        q.offer(new int[]{0, 0});
         visited[0][0] = true;
 
         while (!q.isEmpty()) {
-
             int[] cur = q.poll();
+
             int x = cur[0];
             int y = cur[1];
 
@@ -38,7 +46,7 @@ public class Main {
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
-
+            
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
                     continue;
                 }
@@ -52,14 +60,10 @@ public class Main {
                 }
 
                 visited[nx][ny] = true;
-                q.add(new int[]{nx, ny});
+                q.offer(new int[]{nx, ny});
             }
         }
 
-        if (visited[n-1][m-1]) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+        System.out.println(0);
     }
 }
