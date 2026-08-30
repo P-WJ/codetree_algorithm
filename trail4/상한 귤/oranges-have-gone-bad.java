e14e829c2ad368a3e00390a4e8508a68345506d1
P-WJ <pwj1103@gmail.com>
2026-08-30 16:02:38 +0900

    [Passed] 상한 귤 (110ms, 9MB)

--- trail4/상한 귤/oranges-have-gone-bad.java
+++ trail4/상한 귤/oranges-have-gone-bad.java
@@ -9,26 +9,25 @@ public class Main {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
-    static Queue<int[]> q = new ArrayDeque<>();
-    static boolean[][] visited;
-    static int[][]dist;
-
     public static void main(String[] args) throws Exception {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
+        
         grid = new int[n][n];
-        visited = new boolean[n][n];
-        dist = new int[n][n];
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][n];
+        int[][] dist = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
+
             for (int j = 0; j < n; j++) {
                 grid[i][j] = Integer.parseInt(st.nextToken());
+
                 if (grid[i][j] == 2) {
                     q.offer(new int[]{i, j});
                     visited[i][j] = true;
@@ -36,16 +35,11 @@ public class Main {
             }
         }
 
-
         while (!q.isEmpty()) {
 
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-
-            if (grid[x][y] == 2) {
-                grid[x][y] = 0;
-            }
 
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d];
@@ -63,27 +57,23 @@ public class Main {
                     continue;
                 }
 
-                q.offer(new int[]{nx, ny});
-                visited[nx][ny] = true;
-                
                 dist[nx][ny] = dist[x][y] + 1;
-
+                visited[nx][ny] = true;
+                q.offer(new int[]{nx, ny});
             }
         }
+        
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0 && !visited[i][j]) {
                     dist[i][j] = -1;
-                } 
-                else if (grid[i][j] == 1 && !visited[i][j]) {
+                } else if (grid[i][j] == 1 && !visited[i][j]) {
                     dist[i][j] = -2;
                 }
                 System.out.print(dist[i][j] + " ");
             }
             System.out.println();
-        }
-
-
+        }      
     }
 }
